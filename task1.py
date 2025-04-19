bfd628a0377665b396167976c8c4d0df144fdf10
Omar-Gamall <islorian@hotmail.com>
2025-04-19 22:14:10 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,21 @@
+# explicitly defined variables
+item_1 = 10
+item_2 = 20
+item_3 = 30
+budget = 100
+
+total_cost = item_1 + item_2 + item_3
+difference = budget - total_cost
+
+print(total_cost)
+print(difference)
+
+difference = budget - total_cost
+
+# compare items against budget?
+if total_cost <= budget:
+    print("within budget")
+else:
+    print("over budget")
+
+
